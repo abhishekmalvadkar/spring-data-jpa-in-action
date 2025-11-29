12e76742a4caa89dfd1063a45f3cfb7a156e44d9
Abhishek <abhi@dev.com>
2025-11-29 12:37:46 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/sdjia/SpringDataJpaInActionApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.sdjia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/sdjia/SpringDataJpaInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.sdjia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
